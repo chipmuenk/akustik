fe9cbd86dccbf03c880ada76dcbe0281e5bf2987
chipmuenk <mail@chipmuenk.de>
2018-02-12 22:10:40 +0100

    update to new versions Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- code/_common_imports_m.m
+++ code/_common_imports_m.m
@@ -3,7 +3,7 @@
 % Common Matlab header for "DSV auf FPGA" Code
 %
 % 
-% (c) 2013 Christian Muenker - Files zur Vorlesung "DSV auf FPGAs"
+% (c) 2013 Christian Münker - Files zur Vorlesung "DSV auf FPGAs"
 %================================================================
 set(0,'DefaultAxesColorOrder', [0.8 0 0.2; 0 1 0; 0 0 1], ...
       'DefaultAxesLineStyleOrder','-|--|:|-.');
@@ -15,6 +15,6 @@ set(0,'defaultLineMarkerSize', 6);
 
 set(0,'defaultaxeslinewidth',2);
 set(0,'defaultlinelinewidth',2);
-close all; % alle Plot-Fenster schliessen
-clear all; % alle Variablen aus Workspace loeschen
+close all; % alle Plot-Fenster schließen
+clear all; % alle Variablen aus Workspace löschen
 %
